aa859c9086fd7939584b3f09095f99cafa5cd83a
Nutt Chairatana <nutt@rentspree.com>
2022-06-21 21:32:54 +0700

    Create TasksModule

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,15 @@
+import { Controller } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+  // NOTE: one way to create a dependency injection
+  // tasksService: TasksService;
+
+  // constructor(tasksService: TasksService) {
+  //   this.tasksService = tasksService;
+  // }
+
+  // NOTE: another way to create a dependency injection by using "private" keyword
+  constructor(private tasksService: TasksService) {}
+}
